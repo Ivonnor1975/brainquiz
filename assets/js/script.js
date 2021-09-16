6c6e028f3ec93bfc87692cbfec993053c191feb3
Ivon <ivonne@jjf.com>
2021-09-15 20:14:56 -0500

    display first question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,87 @@
 // Create your HTML Page via DOM Methods here!
-var body = document.body;
-var StartButtonEl = document.createElement("button");
+var StartButtonEl= document.querySelector("#task-form");
+var h1El = document.querySelector(".card h1");
+var h3El = document.querySelector(".card h3");
+var h4El = document.querySelector(".card-footer h4")
+var cardbody=document.querySelector(".card-body");
+var secondsEl=document.querySelector('#seconds');
+var resp='';
+var pageContentEl = document.querySelector(".page-content")
 
 // TODO: Create an array with five question objects
-var questions=[{q:'Commonly used data types DO Not include:', a: '1. Strings',b: '2.	Booleans', c:'3. Alerts ',d: '4.	Numbers', r:3},
-               {q:'The condition in and if / else statement is enclosed with___.', a:'1.	Quotes',b:'2. Curly brackets',c:'3.	Parenthesis', d:'4.	Square brackets', r:3},
-               {q:'Array in Java Script can be used to store ____.', a:'1.	Numbers and strings  ',b: '2. Other arrays', c:'3.	Booleans',d:'4.	All the above', r: 4},
-               {q:'String Values must be enclosed within  ____.', a:'1.	Commas  ',b: '2.	Curly brackets ', c:'3.	Quotes',d:'4.	Parenthesis', r: 3},
-               {q:'A very useful tool used during development and debugging for printing content to the debugger is', a:'1.	JavaScript  ',b: '2.	Terminal/bash ', c:'3.	For loops',d:'4.	Console.log', r: 4}]
+var questions=[{q:'Commonly used data types DO Not include:', a: '1. Strings',b: '2.	Booleans', c:'3. Alerts ',d: '4.	Numbers', r:'3'},
+               {q:'The condition in and if / else statement is enclosed with___.', a:'1.	Quotes',b:'2. Curly brackets',c:'3.	Parenthesis', d:'4.	Square brackets', r:'3'},
+               {q:'Array in Java Script can be used to store ____.', a:'1.	Numbers and strings  ',b: '2. Other arrays', c:'3.	Booleans',d:'4.	All the above', r: '4'},
+               {q:'String Values must be enclosed within  ____.', a:'1.	Commas  ',b: '2.	Curly brackets ', c:'3.	Quotes',d:'4.	Parenthesis', r: '3'},
+               {q:'A very useful tool used during development and debugging for printing content to the debugger is', a:'1.	JavaScript  ',b: '2.	Terminal/bash ', c:'3.	For loops',d:'4.	Console.log', r:'4'}]
     
   
-    // TODO: Create variables to keep track of the score and timer
-    var time=0;
-    var score=0;
+// TODO: Create variables to keep track of the score and timer
+var timer=0;
+var score=0;
 
-var loadpage=function(){
-        // TODO: Add a centered h1
-        var h1 = document.createElement("h1");
-        h1.textContent= "Coding Quiz Challenge";
-        h1.style.textAlign= "center";
-        body.appendChild(h1)
-        // TODO: Add a centered h2
-        var h2 = document.createElement("h2");
-        h2.textContent= "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds.";
-        body.appendChild(h2)
-        // create start quiz button
-        StartButtonEl.textContent = "Start Quiz";
-        StartButtonEl.className = "button";
-        body.appendChild(StartButtonEl);
+// TODO: Iterate over the questions array and display each question in a confirmation box
+var diplayquestions= function(event){   
+       event.preventDefault();   
+       StartButtonEl.remove();
+       h3El.remove();
+       timer=75;
+       secondsEl.textContent=timer;
+       secondsEl.textContent;
+       i=0;
+     
+          // Display a question and list of options
+            resp=questions[i].r;
+            //display question
+            h1El.textContent = questions[i].q;
+            h1El.textContent;
+            var b1El = document.createElement('button');
+                    b1El.textContent= questions[i].a;
+                    b1El.className = "btn1";
+                    b1El.id="bt1"
+                    b1El.setAttribute("data-task-id", 1);
+                    var b2El = document.createElement('button');
+                    b2El.textContent = questions[i].b;
+                    b2El.className = "btn1";
+                    b2El.id="bt2"
+                    b2El.setAttribute("data-task-id", 2);
+                    var b3El = document.createElement('button');
+                    b3El.textContent = questions[i].c;
+                    b3El.className = "btn1";
+                    b3El.id="bt1"
+                    b3El.setAttribute("data-task-id", 3);
+                    var b4El = document.createElement('button');
+                    b4El.textContent = questions[i].d;
+                    b4El.className = "btn1";
+                    b4El.id="bt1"
+                    b4El.setAttribute("data-task-id", 4);
+                    cardbody.appendChild(b1El);
+                    cardbody.appendChild(b2El);
+                    cardbody.appendChild(b3El);
+                    cardbody.appendChild(b4El);
     };
 
-    // TODO: Iterate over the questions array and display each question in a confirmation box
- var diplayquestions= function(){
-        
-       for ( i=0; i < questions.length; i++){
-          var pregunta=questions[i].q;
-          var option1=questions[i].a;
-          var option2=questions[i].b;
-          var option3=questions[i].c;
-          var option4=questions[i].d;
-          // Add  a question and list of options
-            var preguntaEl = document.createElement('div');
-            var listEl = document.createElement('ol');
-            var li1 = document.createElement('li');
-            var li2 = document.createElement('li');
-            var li3 = document.createElement('li');
-            var li4 = document.createElement('li');
-            preguntaEl.textContent = pregunta;
-            li1.textContent = option1;
-            li2.textContent = option2;
-            li3.textContent = option3;
-            li4.textContent = option4;
-            body.appendChild(preguntaEl);
-            preguntaEl.appendChild(listEl);
-            listEl.appendChild(li1);
-            listEl.appendChild(li2);
-            listEl.appendChild(li3);
-            listEl.appendChild(li4);
-                       
+ var taskButtonHandler = function(event) {
+        // get target element from event
+        var targetEl = event.target;
+        var taskId = targetEl.getAttribute("data-task-id");
+        if (taskId=== resp){
+            h4El.textContent = 'Your answer is Correct';
+            h4El.textContent;
+            score++;
+        }else{
+            h4El.textContent = 'Your answer is Wrong';
+            h4El.textContent;
+            timer-=10;
+            if (timer<0){
+                timer=0;
+            }
+            
        }
-    };
-    
-        //     if (useranswer===questions[i].a){
-    //         alert("your answer is correct");
-    //         score++;
-    //     }
-    //     else{
-    //        alert("your answer is wrong");
-    //     }
-    //   };
-    //   alert("You got "+ score +"/" + questions.length);
-loadpage();
+   };
 
 //Section for listeners
-StartButtonEl.addEventListener("click", diplayquestions);
-// Start the quiz
+//start the quiz1
+StartButtonEl.addEventListener("submit", diplayquestions);
+//for list of button options
+pageContentEl.addEventListener("click", taskButtonHandler);
