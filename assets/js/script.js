0ab9ae49923dcd32a536278be7191f5ab79139cd
Ivon <ivonne@jjf.com>
2021-09-15 22:07:46 -0500

    add timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,52 +17,81 @@ var questions=[{q:'Commonly used data types DO Not include:', a: '1. Strings',b:
     
   
 // TODO: Create variables to keep track of the score and timer
-var timer=0;
+var timeleft=0;
 var score=0;
+
+var loadpage=function(){
+            h1El.textContent="Coding Quiz Challenge";
+            h1El.textContent;
+            h3El.textContent="Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds.";
+};
 
 // TODO: Iterate over the questions array and display each question in a confirmation box
 var diplayquestions= function(event){   
        event.preventDefault();   
        StartButtonEl.remove();
        h3El.remove();
-       timer=75;
-       secondsEl.textContent=timer;
-       secondsEl.textContent;
+       timeleft=25;
+       secondsEl.textContent=timeleft;
        i=0;
-     
-          // Display a question and list of options
-            resp=questions[i].r;
-            //display question
-            h1El.textContent = questions[i].q;
-            h1El.textContent;
-            var b1El = document.createElement('button');
-                    b1El.textContent= questions[i].a;
-                    b1El.className = "btn1";
-                    b1El.id="bt1"
-                    b1El.setAttribute("data-task-id", 1);
-                    var b2El = document.createElement('button');
-                    b2El.textContent = questions[i].b;
-                    b2El.className = "btn1";
-                    b2El.id="bt2"
-                    b2El.setAttribute("data-task-id", 2);
-                    var b3El = document.createElement('button');
-                    b3El.textContent = questions[i].c;
-                    b3El.className = "btn1";
-                    b3El.id="bt1"
-                    b3El.setAttribute("data-task-id", 3);
-                    var b4El = document.createElement('button');
-                    b4El.textContent = questions[i].d;
-                    b4El.className = "btn1";
-                    b4El.id="bt1"
-                    b4El.setAttribute("data-task-id", 4);
-                    cardbody.appendChild(b1El);
-                    cardbody.appendChild(b2El);
-                    cardbody.appendChild(b3El);
-                    cardbody.appendChild(b4El);
-    };
+       loadnextquestion(i);
+       // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+        var timeInterval = setInterval(function() {
+                // As long as the `timeLeft` is greater than 1
+                if (timeleft > 1) {
+                    // Set the `textContent` of `timerEl` to show the remaining seconds
+                    secondsEl.textContent = timeleft;
+                    // Decrement `timeLeft` by 1
+                    timeleft--;
+                } else if (timeleft === 1) {
+                    // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+                    secondsEl.textContent = timeleft;
+                    timeleft--;
+                } else {
+                    // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+                    secondsEl.textContent = '0';
+                    // Use `clearInterval()` to stop the timer
+                    clearInterval(timeInterval);
+                    // Call the record score function
+                    //recordScore();
+                }
+                
+        }, 1000);
+     };
 
+var loadnextquestion=function(c){
+               // Display a question and list of options
+               resp=questions[c].r;
+               //display question
+               h1El.textContent = questions[c].q;
+               h1El.textContent;
+               var b1El = document.createElement('button');
+               b1El.textContent= questions[c].a;
+               b1El.className = "btn1";
+               b1El.id="bt1"
+               b1El.setAttribute("data-task-id", 1);
+               var b2El = document.createElement('button');
+               b2El.textContent = questions[c].b;
+               b2El.className = "btn1";
+               b2El.id="bt2"
+               b2El.setAttribute("data-task-id", 2);
+               var b3El = document.createElement('button');
+               b3El.textContent = questions[c].c;
+               b3El.className = "btn1";
+               b3El.id="bt3"
+               b3El.setAttribute("data-task-id", 3);
+               var b4El = document.createElement('button');
+               b4El.textContent = questions[c].d;
+               b4El.className = "btn1";
+               b4El.id="bt4"
+               b4El.setAttribute("data-task-id", 4);
+               cardbody.appendChild(b1El);
+               cardbody.appendChild(b2El);
+               cardbody.appendChild(b3El);
+               cardbody.appendChild(b4El);
+};
  var taskButtonHandler = function(event) {
-        // get target element from event
+        // get target element from event what option was selected
         var targetEl = event.target;
         var taskId = targetEl.getAttribute("data-task-id");
         if (taskId=== resp){
@@ -72,13 +101,20 @@ var diplayquestions= function(event){
         }else{
             h4El.textContent = 'Your answer is Wrong';
             h4El.textContent;
-            timer-=10;
-            if (timer<0){
-                timer=0;
-            }
-            
-       }
-   };
+            timeleft-=10;
+        }
+        if (timeleft<0){
+            timeleft=0;
+        }
+       i++;
+       document.querySelector('#bt1').remove();
+       document.querySelector('#bt2').remove();
+       document.querySelector('#bt3').remove();
+       document.querySelector('#bt4').remove();
+       loadnextquestion(i); 
+ };
+
+loadpage();
 
 //Section for listeners
 //start the quiz1
